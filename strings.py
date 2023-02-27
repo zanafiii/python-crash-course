f9abaafac959a9b700c75e862a6f97b5a522cdd5
Azzam Hanafi <azzam.emirates@gmail.com>
2023-02-27 17:50:52 +0700

    add strings.ph

---
+++ strings.py
@@ -0,0 +1,5 @@
+say = " yes "
+print(say)
+print(say.strip())      # Remove all whitespace
+print(say.lstrip())     # Remove all whitespace on the left
+print(say.rstrip())     # Remove all whitespace on the right
